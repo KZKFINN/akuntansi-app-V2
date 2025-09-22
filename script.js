3f8ff1d913ad08b4354577ad7275ade601d15ab5
KZKFINN <alfinalpiyan3@gmail.com>
2025-09-22 22:13:15 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById("app").innerHTML = `
+  <div class='card'>
+    <h2>Akuntansi Pro Modern</h2>
+    <p>Aplikasi akuntansi siap pakai.</p>
+    <button onclick="alert('Tambah transaksi!')">+ Tambah Transaksi</button>
+  </div>
+`;
